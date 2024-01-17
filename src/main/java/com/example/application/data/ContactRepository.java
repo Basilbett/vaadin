21231084678d50fcce163e3ca6eeb1d69e83c6be
sharonchebet <basil@kabarak.ac.ke>
2024-01-17 19:26:08 +0300

    hand on

--- src/main/java/com/example/application/data/ContactRepository.java
+++ src/main/java/com/example/application/data/ContactRepository.java
@@ -3,6 +3,21 @@ package com.example.application.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ContactRepository extends JpaRepository<Contact, Long> {
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+
+import java.util.List;
+
+
+public interface ContactRepository extends JpaRepository<Contact, Integer> {
+
+ @Query("select c from Contact c" +
+         "where lower (c.firstName)like lower (contact('%', :searchTerm , '%'))" +
+         "or lower(c.lastName)like lower (contact('%',:searchTerm,'%'))")
+ List<Contact> search (@Param("searchTerm") String searchTerm);
+
+
+
 
 }
--- src/main/java/com/example/application/views/list/ListView.java
+++ src/main/java/com/example/application/views/list/ListView.java
@@ -1,33 +1,97 @@
 package com.example.application.views.list;
 
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
+import com.example.application.data.Contact;
+import com.example.application.services.CrmService;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
 
 @PageTitle("list")
 @Route(value = "")
 public class ListView extends VerticalLayout {
 
-    public ListView() {
-        setSpacing(false);
+    private final CrmService service;
+    Grid<Contact> grid=new Grid<> (Contact.class);
+    TextField filterText=new TextField();
 
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
-
-        H2 header = new H2("This place intentionally left empty");
-        header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
-        add(header);
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
-
+         ContactForm form;
+    public ListView( CrmService service) {
+        this.service=service;
+        addClassName("list-view");
         setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center");
+
+        configureGrid();
+        ConfigureForm();
+
+       add(
+               getToolbar(),
+
+               GetContent()
+
+               
+
+       );
+       updateList();
+    }
+
+    private void updateList() {
+        grid.setItems(service.findAllContacts(filterText.getValue()));
+    }
+
+    private Component GetContent() {
+        HorizontalLayout content =new HorizontalLayout(grid,form);
+        content.setFlexGrow(2,grid);
+        content.setFlexGrow(1,form);
+        content.addClassName("content");
+        content.setSizeFull();
+
+        return content;
+
+    }
+
+    private void ConfigureForm() {
+
+        form= new ContactForm(service.findAllCompanies(),service.findAllStatues());
+        form.setWidth("25em");
+    }
+
+    private Component getToolbar() {
+
+
+        filterText.setPlaceholder("Filter by name...");
+        filterText.setClearButtonVisible(true);
+        filterText.setValueChangeMode(ValueChangeMode.LAZY);
+
+        filterText.addValueChangeListener(e ->updateList());
+
+        Button addContactButton =new Button("add Contact");
+        HorizontalLayout toolbar=new HorizontalLayout(filterText,addContactButton);
+        toolbar.addClassName("toolbar");
+
+
+
+        return toolbar;
+
+
+
+
+    }
+
+    private void configureGrid() {
+        addClassName("contact-grid");
+        setSizeFull();
+
+        grid.setColumns("firstName" ,"lastName","email");
+        grid.addColumn(contact->contact.getCompany().getName()).setHeader("Company");
+        grid.addColumn(contact->contact.getStatus().getName()).setHeader("Status");
+
+        grid.getColumns().forEach(column ->column.setAutoWidth(true));
     }
 
 }
